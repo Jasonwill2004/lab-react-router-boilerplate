7f23503ee80d247071fbf5f1adebc1fc66a77c96
jason william <jasonwilliam@jasons-MacBook-Air.local>
2024-03-07 11:42:03 +0530

    done

---
+++ routing/src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../index.css"
+
+const About = () => {
+  return (
+    <div style={{marginTop:"200px"}} >
+      <center  >
+        <h1 style={{fontSize:"50px",color:"white"}} >About Us </h1>
+      </center>
+    </div>
+  )
+}
+
+export default About
